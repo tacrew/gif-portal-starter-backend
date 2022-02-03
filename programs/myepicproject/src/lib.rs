8308e89b72be7f2d502fe7f592ec908429bd5388
tacrew <t.murakami411@gmail.com>
2022-02-03 20:12:59 +0900

    init

---
+++ programs/myepicproject/src/lib.rs
@@ -0,0 +1,60 @@
+use anchor_lang::prelude::*;
+
+declare_id!("914vKswXzCLBvGHcJXaCcZ1soRVZMY7y7nCFTf3fozaW");
+
+#[program]
+pub mod myepicproject {
+  use super::*;
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+    let base_account = &mut ctx.accounts.base_account;
+    base_account.total_gifs = 0;
+    Ok(())
+  }
+
+  pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
+    let base_account = &mut ctx.accounts.base_account;
+    let user = &mut ctx.accounts.user;
+
+    let item = ItemStruct {
+      gif_link: gif_link.to_string(),
+      likes: 0,
+      user_address: *user.to_account_info().key,
+    };
+
+    base_account.gif_list.push(item);
+    base_account.total_gifs += 1;
+    Ok(())
+  }
+
+// Attach certain variables to the StartStuffOff context.
+#[derive(Accounts)]
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+  pub gif_link: String,
+  pub likes: u64,
+  pub user_address: Pubkey,
+}
+
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+    pub gif_list: Vec<ItemStruct>,
+}
